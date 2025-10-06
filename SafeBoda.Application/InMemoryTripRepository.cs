17832f2b96f2fb10cb788cc737bd69e693415a4a
Manzp111 <gilbertnshimyimana11@gmail.com>
2025-10-06 13:29:57 +0200

    adding models as record and business logic as interface

---
+++ SafeBoda.Application/InMemoryTripRepository.cs
@@ -0,0 +1,12 @@
+namespace InMemoryTripRepository
+
+public class InMemoryTripRepository : ITripRepository
+{ 
+    public IEnumerable GetActiveTrips() { 
+        return new List {
+            
+            
+            
+        }; 
+    } 
+}
