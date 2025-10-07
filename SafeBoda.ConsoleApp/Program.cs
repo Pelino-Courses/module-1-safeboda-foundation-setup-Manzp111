e2ed7178096c447afc48700af3919eb42c0121dd
Manzp111 <gilbertnshimyimana11@gmail.com>
2025-10-07 17:00:28 +0200

    adding SafeBoda console app for testing

---
+++ SafeBoda.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using SafeBoda.Application;
+
+var repo = new InMemoryTripRepository();
+var trips = repo.GetActiveTrips();
+
+foreach (var trip in trips)
+{
+    Console.WriteLine($"Trip ID: {trip.Id} | Fare: {trip.Fare} | From: ({trip.Start.Latitude},{trip.Start.Longitude}) to ({trip.End.Latitude},{trip.End.Longitude})");
+}
